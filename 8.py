8c121ed65697f31d35cc0273432ffdbe7e30be1e
michaeldavie <michael.davie@gmail.com>
2018-12-08 17:39:01 -0500

    Day 8

---
+++ 8.py
@@ -0,0 +1,47 @@
+from collections import deque
+
+with open('8.input', 'r') as input_file:
+    input_deque = deque(map(int, input_file.read().split(' ')))
+
+# Part 1
+
+def sum_meta(data_deque):
+    children = data_deque.popleft()
+    metas = data_deque.popleft()
+    meta_sum = 0
+
+    for children in range(0, children):
+        meta_sum += sum_meta(data_deque)
+
+    meta_sum += sum([data_deque.popleft() for m in range(0, metas)])
+
+    return meta_sum
+
+print(sum_meta(input_deque.copy()))
+
+# Part 2
+
+def node_value(data_deque):
+    children = data_deque.popleft()
+    meta_count = data_deque.popleft()
+
+    value = 0
+    child_values = []
+
+    if children > 0:
+        for child in range(0, children):
+            child_values.append(node_value(data_deque))
+
+    meta_values = [data_deque.popleft() for m in range(0, meta_count)]
+
+    if children == 0:
+        return sum(meta_values)
+
+    for m in meta_values:
+        if m == 0 or m > children:
+            continue
+        value += child_values[m - 1]
+
+    return value
+
+print(node_value(input_deque.copy()))
